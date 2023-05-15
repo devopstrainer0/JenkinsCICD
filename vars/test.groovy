a8c6db6052126b3530f991bfdfa669c06115e414
devopstrainer0 <122166097+devopstrainer0@users.noreply.github.com>
2023-05-15 12:28:02 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -8,10 +8,21 @@ def call(Map params = [:]) {
     }
   */
   for (def key in params.inputScan.keySet()) {
+       def cList = []
+       def bList = []
         echo "Key: ${key}"
         for (def value in params.inputScan.get(key)) {
+           
             echo "Value: ${value}"
+           for 
         }
     }
+   
+   for (def i = 0; i < params.inputScan.get(key)[0].size(); i++) {
+         def value1 = params.inputScan.get(key)[0][i]
+         def value2 = params.inputScan.get(key)[1][i]
+         echo "Value 1: ${value1}"
+         echo "Value 2: ${value2}"
+      }
   
 }
