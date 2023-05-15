ab2f452bad4d8bd12bbb3fb3df20dc59aaa1d130
devopstrainer0 <122166097+devopstrainer0@users.noreply.github.com>
2023-05-15 20:18:34 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,49 +1,40 @@
 def call(Map params = [:]) {
-
+   
     println params
-    echo "*********************************************"
+    println "*********************************************"
     println "Name: ${params.name}"
     println "Number: ${params.number}"
-    
+   
     for (def itr in params.inputScan.keySet()) {
         def cList = []
         def bList = []
-        echo "Key: ${itr}"
+        println "Key: ${itr}"
         def count = 0
         for (def value in params.inputScan.get(itr)) {
             if (count == 0) {
                 println "The count is 0!"
-                echo "Value: ${value}"
-                cList = Eval.me(value)
-                echo "cList: ${cList}"
+                println "Value: ${value}"
+                cList = value
+                println "cList: ${cList}"
                 count++
             } else {
                 println "The count is 1!"
-                echo "Value: ${value}"
-                bList = Eval.me(value)
-                echo "bList: ${bList}"
+                println "Value: ${value}"
+                bList = value
+                println "bList: ${bList}"
             }
-            
+        }
+        
+        if (cList.size() == bList.size()) {
             for (i = 0; i < cList.size(); i++) {
-                params.each { tkey, value ->
-                    if (tkey == 'giturl') {
-                        params.put(tkey, "${itr}")
-                    }
-                    
-                    if (tkey == 'compname') {
-                        params.put(tkey, cList[i])
-                    }
-                    
-                    if (tkey == 'buildfile') {
-                        params.put(tkey, bList[i])
-                    }
-                }
-                
-                echo "Params: ${params}"
+                params.compname = cList[i]
+                params.buildfile = bList[i]
+                params.giturl = itr
+                println "Updated params: ${params}"
             }
         }
     }
     
-    echo "*********************************************"
+    println "*********************************************"
     println params
 }
