2033166b05e111d68deff81ea1a83ae6a1f870c3
devopstrainer0 <122166097+devopstrainer0@users.noreply.github.com>
2023-05-15 12:32:57 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -11,17 +11,20 @@ def call(Map params = [:]) {
        def cList = []
        def bList = []
         echo "Key: ${key}"
+        def count = 0
         for (def value in params.inputScan.get(key)) {
            
             echo "Value: ${value}"
+           if (count == 0) {
+                println "The count is 0!"
+                count++
+               }
+            if (count == 1) {
+                println "The count is 1!"
+               
+               }
         }
     }
-   
-   for (def i = 0; i < params.inputScan.get(key)[0].size(); i++) {
-         def value1 = params.inputScan.get(key)[0][i]
-         def value2 = params.inputScan.get(key)[1][i]
-         echo "Value 1: ${value1}"
-         echo "Value 2: ${value2}"
-      }
+
   
 }
