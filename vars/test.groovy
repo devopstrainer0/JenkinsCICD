3e2f09d831399187d0d6515ad864e3c60b54b551
devopstrainer0 <122166097+devopstrainer0@users.noreply.github.com>
2023-05-15 19:47:44 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,57 +1,46 @@
+
 def call(Map params = [:]) {
-   
-   println params
-   echo "*********************************************"
-   println "Name: ${params.name}"
+    println params
+    echo "*********************************************"
+    println "Name: ${params.name}"
     println "Number: ${params.number}"
-    
-   
- 
-  for (def itr in params.inputScan.keySet()) {
-       def cList = []
-      def bList = []
+    for (def itr in params.inputScan.keySet()) {
+        def cList = []
+        def bList = []
         echo "Key: ${itr}"
         def count = 0
         for (def value in params.inputScan.get(itr)) {
-           if (count == 0) {
+            if (count == 0) {
                 println "The count is 0!"
-                  echo "Value: ${value}"
+                echo "Value: ${value}"
                 cList = value
-         
-              echo "cList: ${cList}"
+                echo "cList: ${cList}"
                 count++
-                   }else {
+            } else {
                 println "The count is 1!"
-                  echo "Value: ${value}"
-              bList = value
-              
+                echo "Value: ${value}"
+                bList = value
                 echo "bList: ${bList}"
-               }
-               
-               params.each { key, value ->
-       					 if (key== 'giturl'){
-        					params.put(key,"${itr}")
-        					}
-       					
-    			}
-               
-               for (i=0; i < cList.size(); i++ ){
-               			params.each { key, value ->
-       					 if (key== 'compname'){
-        					params.put(key,cList[i])
-        					}
-       					  if (key== 'buildfile'){
-        					params.put(key,bList[i])
-        			}
-    			}
-               
-               }
+            }
+            params.each { gkey, val ->
+                if (gkey == 'giturl') {
+                    params.put(gkey, "${itr}")
+                }
+            }
+            if (cList.size() == bList.size()) {
+                for (i = 0; i < cList.size(); i++) {
+                    params.each { key, val ->
+                        if (key == 'compname') {
+                            params.put(key, cList[i])
+                        }
+                        if (key == 'buildfile') {
+                            params.put(key, bList[i])
+                        }
+                    }
+                }
+            }
         }
-        
-           
-   echo "*********************************************"
-   println params
     }
-
-  
+    echo "*********************************************"
+    println params
 }
